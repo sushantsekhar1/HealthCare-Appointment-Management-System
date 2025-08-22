09bfa0b3efa1504b74b3fb22596a6060bfceacab
Sushant Sekhar <57630756+sushantsekhar1@users.noreply.github.com>
2025-08-22 19:16:04 +0530

    Update RelatedDoctors.jsx

--- frontend/src/components/RelatedDoctors.jsx
+++ frontend/src/components/RelatedDoctors.jsx
@@ -1,3 +1,5 @@
+
+
 import React, { useContext, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { AppContext } from '../context/AppContext'
@@ -21,13 +23,13 @@ const RelatedDoctors = ({ speciality, docId }) => {
             <p className='sm:w-1/3 text-center text-sm text-blue-400'>Simply browse through our extensive list of trusted doctors.</p>
             <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
                 {relDoc.map((item, index) => (
-                    <div onClick={() => { navigate(`/appointment/${item._id}`); scrollTo(0, 0) }} className='border border-[#C9D8FF] rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
+                    <div onClick={() => { navigate(`/appointment/${item._id}`); scrollTo(0, 0) }} className='border border-[#C9D8FF] rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500 ' key={index}>
                         <img className='bg-[#EAEFFF]' src={item.image} alt="" />
                         <div className='p-4'>
                             <div className={`flex items-center gap-2 text-sm text-center ${item.available ? 'text-green-500' : "text-gray-500"}`}>
                                 <p className={`w-2 h-2 rounded-full ${item.available ? 'bg-green-500' : "bg-gray-500"}`}></p><p>{item.available ? 'Available' : "Not Available"}</p>
                             </div>
-                            <p className='text-[#262626] text-lg font-medium'>{item.name}</p>
+                            <p className='text-[#262626] text-lg font-medium '>{item.name}</p>
                             <p className='text-[#5C5C5C] text-sm'>{item.speciality}</p>
                         </div>
                     </div>
