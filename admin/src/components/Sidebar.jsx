793a33976a738939efd6f8aa184e7683a1f256e3
Sushant Sekhar <57630756+sushantsekhar1@users.noreply.github.com>
2025-08-22 18:46:19 +0530

    Update Sidebar.jsx

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -1,56 +1,3 @@
-// import React, { useContext } from 'react'
-// import { assets } from '../assets/assets'
-// import { NavLink } from 'react-router-dom'
-// import { DoctorContext } from '../context/DoctorContext'
-// import { AdminContext } from '../context/AdminContext'
-
-// const Sidebar = () => {
-
-//   const { dToken } = useContext(DoctorContext)
-//   const { aToken } = useContext(AdminContext)
-
-//   return (
-//     <div className='min-h-screen bg-white border-r'>
-//       {aToken && <ul className='text-[#515151] mt-5'>
-
-//         <NavLink to={'/admin-dashboard'} className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}`}>
-//           <img className='min-w-5' src={assets.home_icon} alt='' />
-//           <p className='hidden md:block'>Dashboard</p>
-//         </NavLink>
-//         <NavLink to={'/all-appointments'} className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}`}>
-//           <img className='min-w-5' src={assets.appointment_icon} alt='' />
-//           <p className='hidden md:block'>Appointments</p>
-//         </NavLink>
-//         <NavLink to={'/add-doctor'} className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}`}>
-//           <img className='min-w-5' src={assets.add_icon} alt='' />
-//           <p className='hidden md:block'>Add Doctor</p>
-//         </NavLink>
-//         <NavLink to={'/doctor-list'} className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}`}>
-//           <img className='min-w-5' src={assets.people_icon} alt='' />
-//           <p className='hidden md:block'>Doctors List</p>
-//         </NavLink>
-//       </ul>}
-
-//       {dToken && <ul className='text-[#515151] mt-5'>
-//         <NavLink to={'/doctor-dashboard'} className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}`}>
-//           <img className='min-w-5' src={assets.home_icon} alt='' />
-//           <p className='hidden md:block'>Dashboard</p>
-//         </NavLink>
-//         <NavLink to={'/doctor-appointments'} className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}`}>
-//           <img className='min-w-5' src={assets.appointment_icon} alt='' />
-//           <p className='hidden md:block'>Appointments</p>
-//         </NavLink>
-//         <NavLink to={'/doctor-profile'} className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}`}>
-//           <img className='min-w-5' src={assets.people_icon} alt='' />
-//           <p className='hidden md:block'>Profile</p>
-//         </NavLink>
-//       </ul>}
-//     </div>
-//   )
-// }
-
-// export default Sidebar
-
 import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 import { assets } from '../assets/assets';
@@ -75,7 +22,7 @@ const Sidebar = () => {
   );
 
   return (
-    <aside className="min-h-screen w-full md:w-64 bg-white border-r shadow-lg p-4 flex flex-col gap-4 ">
+    <aside className="min-h-screen w-full md:w-64 border-r shadow-lg p-4 flex flex-col gap-4 ">
       {/* Sidebar header/logo */}
       <nav className="flex flex-col gap-2">
         {aToken && (
